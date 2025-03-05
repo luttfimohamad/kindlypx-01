ca07a56a1b7afc798f2a1eb952875b99217f0a2d
luttfimohamad <luttfimohamad@gmail.com>
2025-03-05 22:42:28 +0700

    Add Todo component and update App layout with styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import Todo from './components/Todo';
+
 export default function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>MY TODOS</h1>
+      <Todo />
+    </div>
+  );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Todo() {
+  return (
+    <div className="card">
+      <h1>TITLE</h1>
+      <div className="actions">
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
+}
