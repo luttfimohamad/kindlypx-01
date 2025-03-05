2c6c9fd33f6b158c23d7bf664b22b992c8f39b72
luttfimohamad <luttfimohamad@gmail.com>
2025-03-05 23:01:52 +0700

    Add delete handler to Todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 export default function Todo(props) {
+  function deleteHandler() {
+    console.log('Delete handler');
+  }
+
   return (
     <div className="card">
       <h1>{props.title}</h1>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
